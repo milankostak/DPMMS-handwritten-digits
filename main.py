e6864789efd138f2053cef3e17f6f09d75568c39
Milan Košťák <milankostak@seznam.cz>
2019-05-01 21:45:54 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import math
+import mnist
+import ploting
+
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
+
+train_images = train_images.reshape(60000, 28, 28, 1).astype('float32')
+test_images = test_images.reshape(10000, 28, 28, 1).astype('float32')
+
+# normalize data
+train_images /= 255
+test_images /= 255
+
+num_train_examples = train_images.shape[0]
+num_test_examples = test_images.shape[0]
+
+# check if data labeling is correct
+ploting.plot_init(train_images, train_labels)
+
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+#     tf.keras.layers.Dense(128, activation=tf.nn.relu),
+#     tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+# ])
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation=tf.nn.relu, input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+    tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation=tf.nn.relu),
+    tf.keras.layers.MaxPooling2D((2, 2), strides=2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(32, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+BATCH_SIZE = 1000
+
+model.fit(x=train_images, y=train_labels, epochs=5, batch_size=BATCH_SIZE)
+# model.fit(x=train_images, y=train_labels, epochs=5, steps_per_epoch=math.ceil(num_train_examples / BATCH_SIZE))
+
+# EVALUATE
+
+test_loss, test_accuracy = model.evaluate(x=test_images, y=test_labels, steps=math.ceil(num_test_examples / BATCH_SIZE))
+
+print('\nAccuracy on test dataset:', test_accuracy)
+
+predictions = model.predict(test_images)
+ploting.plot_result(predictions, test_images, test_labels)
---
+++ ploting.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_init(images, labels):
+    plt.figure(figsize=(10, 10))
+    for i in range(0, 25):
+        plt.subplot(5, 5, i + 1)
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(images[i].reshape((28, 28)), cmap=plt.cm.binary)
+        plt.xlabel(labels[i], fontsize=24)
+    plt.show()
+
+
+def plot_result(predictions, test_images, test_labels):
+    rows = 5
+    cols = 3
+    plt.figure(figsize=(2 * 2 * cols, 2 * rows))
+    for i in range(rows * cols):
+        plt.subplot(rows, 2 * cols, 2 * i + 1)
+        plot_image(i, predictions, test_labels, test_images)
+        plt.subplot(rows, 2 * cols, 2 * i + 2)
+        plot_value_array(i, predictions, test_labels)
+    plt.show()
+
+
+def plot_image(index, predictions, test_labels, test_images):
+    prediction, true_label, img = predictions[index], test_labels[index], test_images[index]
+
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(img.reshape((28, 28)), cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(prediction)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(predicted_label,
+                                         100 * np.max(prediction),
+                                         true_label),
+               color=color, fontsize=24)
+
+
+def plot_value_array(index, predictions, test_labels):
+    prediction, true_label = predictions[index], test_labels[index]
+
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    this_plot = plt.bar(range(10), prediction, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(prediction)
+
+    this_plot[predicted_label].set_color('red')
+    this_plot[true_label].set_color('blue')
